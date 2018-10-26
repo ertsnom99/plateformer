6e9ce8825173b56a136ec18cd880b45019b815e0
Kévin Duguay <kevin.duguay1@gmail.com>
2018-10-26 00:23:00 -0400

    Added jump

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -4,8 +4,17 @@ public class CharacterController : MonoBehaviour
 {
     [SerializeField]
     private float m_maxSpeed = 100.0f;
+    [SerializeField]
+    private Transform m_groundCheck;
+    [SerializeField]
+    private float m_groundRadius = .2f;
+    [SerializeField]
+    private LayerMask m_groundLayer;
+    [SerializeField]
+    private float m_jumpForce = 700.0f;
 
     private bool m_isFacingRight = true;
+    private bool m_isGrounded = false;
 
     private Rigidbody2D m_rigidbody2D;
     private Animator m_animator;
@@ -18,10 +27,16 @@ public class CharacterController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        float move = Input.GetAxis("Horizontal");
-        
-        m_rigidbody2D.velocity = new Vector2(move * m_maxSpeed * Time.fixedDeltaTime, m_rigidbody2D.velocity.y);
+        m_isGrounded = Physics2D.OverlapCircle(m_groundCheck.position, m_groundRadius, m_groundLayer);
 
+        float move = Mathf.Ceil(Input.GetAxis("Horizontal"));
+        
+        //Movement instantanious 
+        m_rigidbody2D.AddForce(new Vector2(move * m_maxSpeed * Time.fixedDeltaTime, m_rigidbody2D.velocity.y) - m_rigidbody2D.velocity, ForceMode2D.Impulse);
+        //m_rigidbody2D.velocity = new Vector2(move * m_maxSpeed * Time.fixedDeltaTime, m_rigidbody2D.velocity.y);
+        //Movement with acceleration/decceleration
+        //m_rigidbody2D.AddForce(new Vector2(move * m_maxSpeed * Time.fixedDeltaTime, m_rigidbody2D.velocity.y) - m_rigidbody2D.velocity, ForceMode2D.Force);
+        
         if (move > .0f && !m_isFacingRight)
         {
             Flip();
@@ -32,6 +47,19 @@ public class CharacterController : MonoBehaviour
         }
 
         m_animator.SetFloat("Speed", Mathf.Abs(move));
+        m_animator.SetBool("IsGrouned", m_isGrounded);
+        m_animator.SetFloat("VerticalSpeed", m_rigidbody2D.velocity.y);
+    }
+
+    private void Update()
+    {
+        if (m_isGrounded && Input.GetButtonDown("Jump"))
+        {
+            m_rigidbody2D.AddForce(new Vector2(.0f, m_jumpForce));
+
+            m_isGrounded = false;
+            m_animator.SetBool("IsGrouned", m_isGrounded);
+        }
     }
 
     private void Flip()
@@ -40,5 +68,12 @@ public class CharacterController : MonoBehaviour
         Vector3 newScale = transform.localScale;
         newScale.x *= -1;
         transform.localScale = newScale;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        // Display the explosion radius when selected
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(m_groundCheck.position, m_groundRadius);
     }
 }
