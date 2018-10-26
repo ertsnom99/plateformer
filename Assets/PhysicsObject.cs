0a861aee22eb437470a2f53f2fbd3687519c7290
Kévin Duguay <kevin.duguay1@gmail.com>
2018-10-26 16:59:44 -0400

    Added Y movement throw code

---
+++ Assets/PhysicsObject.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsObject : MonoBehaviour
+{
+    [SerializeField]
+    private float m_gravityModifier = 1.0f;
+    [SerializeField]
+    private float m_minGroundNormalY = .65f;
+
+    protected bool m_isGrounded = false;
+    protected Vector2 m_groundNormal;
+
+    protected Vector2 m_velocity;
+    protected ContactFilter2D m_contactFilter;
+    protected RaycastHit2D[] m_hitBuffer = new RaycastHit2D[16];
+    protected List<RaycastHit2D> m_hitBufferList = new List<RaycastHit2D>(16);
+    
+    protected Rigidbody2D m_rigidbody2D;
+
+    protected const float MinMoveDistance = 0.001f;
+    protected const float ShellRadius = 0.01f;
+
+    private void Awake()
+    {
+        m_contactFilter.useTriggers = false;
+        // Tells to use the layer settings from the Physics2D settings (the matrix)
+        m_contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer));
+        m_contactFilter.useLayerMask = true;
+
+        m_rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        m_isGrounded = false;
+
+        // Update velocity
+        m_velocity += m_gravityModifier * Physics2D.gravity * Time.fixedDeltaTime;
+        
+        // Calculate movement to be done
+        Vector2 movement = Vector2.up * (m_velocity * Time.fixedDeltaTime).y;
+
+        Move(movement, true);
+    }
+
+    private void Move(Vector2 movement, bool yMovement)
+    {
+        float distance = movement.magnitude;
+
+        // Check for collision only if the object moves enough
+        if (distance > MinMoveDistance)
+        {
+            int count = m_rigidbody2D.Cast(movement, m_contactFilter, m_hitBuffer, distance + ShellRadius);
+            m_hitBufferList.Clear();
+
+            // Transfer hits to m_hitBufferList
+            for (int i = 0; i < count; i++)
+            {
+                m_hitBufferList.Add(m_hitBuffer[i]);
+            }
+
+            foreach (RaycastHit2D hit in m_hitBufferList)
+            {
+                Vector2 currentNormal = hit.normal;
+
+                // Check if the object is grounded
+                if (currentNormal.y > m_minGroundNormalY)
+                {
+                    m_isGrounded = true;
+
+                    if (yMovement)
+                    {
+                        m_groundNormal = currentNormal;
+                        currentNormal.x = 0;
+                    }
+                }
+
+                // Check how much the object is going to go throw other colliders
+                float projection = Vector2.Dot(m_velocity, currentNormal);
+
+                if (projection < .0f)
+                {
+                    // Remove part of the velocity to prevent from going throw colliders
+                    m_velocity -= projection * currentNormal;
+                }
+
+                // Calculate how much movement can be done, before touching the ground, considering the ShellRadius  
+                float modifiedDistance = hit.distance - ShellRadius;
+                // If, after calculation, we should move less then what we tought at first, then move less
+                distance = modifiedDistance < distance ? modifiedDistance : distance;
+            }
+        }
+
+        // Apply the movement
+        m_rigidbody2D.position = m_rigidbody2D.position + movement.normalized * distance;
+    }
+}
