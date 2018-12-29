29f79d668e9e10924c369423cb2b57ad7bb44b56
Kévin Duguay <kevin.duguay1@gmail.com>
2018-12-28 19:00:34 -0500

    Added health bar

---
+++ Assets/Scripts/Character/Health.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+
+public interface IHealthSubscriber
+{
+    void NotifyJustSubscribed(Health healthScript);
+    void NotifyDamageApplied(Health healthScript, int damage);
+    void NotifyHealthChange(Health healthScript, int health);
+    void NotifyHealthDepleted(Health healthScript);
+}
+
+public interface IDamageable
+{
+    void Damage(int damage);
+}
+
+public interface IHealable
+{
+    void Heal(int gain);
+}
+
+public class Health : MonoSubscribable<IHealthSubscriber>, IDamageable, IHealable
+{
+    [Header("Health")]
+    [SerializeField]
+    private int m_maxHealth = 100;
+    public int MaxHealth
+    {
+        get { return m_maxHealth; }
+        private set { m_maxHealth = value; }
+    }
+
+    public int HealthPoint { get; protected set; }
+
+    private float m_invulnerabilityDuration;
+    private float m_invulnerabilityStartTime;
+
+    public bool IsInvulnerability { get; private set; }
+
+    protected virtual void Awake()
+    {
+        HealthPoint = MaxHealth;
+        IsInvulnerability = false;
+    }
+
+    protected virtual void Update()
+    {
+        // Update invulnerability
+        if (IsInvulnerability && (Time.time - m_invulnerabilityStartTime) > m_invulnerabilityDuration)
+        {
+            IsInvulnerability = false;
+        }
+    }
+
+    public void StartInvulnerability(float duration = Mathf.Infinity)
+    {
+        IsInvulnerability = true;
+
+        m_invulnerabilityStartTime = Time.time;
+        m_invulnerabilityDuration = duration;
+    }
+
+    public void StopInvulnerability()
+    {
+        IsInvulnerability = false;
+    }
+
+    protected virtual void OnDamageApplied() { }
+
+    protected virtual void OnHealthDepleted() { }
+
+    // Methods of the IDamageable interface
+    public virtual void Damage(int damage)
+    {
+        foreach (IHealthSubscriber subscriber in m_subscribers)
+        {
+            subscriber.NotifyDamageApplied(this, damage);
+        }
+
+        if (!IsInvulnerability && damage > 0)
+        {
+            HealthPoint = HealthPoint - damage < 0 ? 0 : HealthPoint - damage;
+
+            OnDamageApplied();
+
+            if (HealthPoint == 0)
+            {
+                OnHealthDepleted();
+            }
+
+            foreach (IHealthSubscriber subscriber in m_subscribers)
+            {
+                subscriber.NotifyHealthChange(this, HealthPoint);
+
+                if (HealthPoint == 0)
+                {
+                    subscriber.NotifyHealthDepleted(this);
+                }
+            }
+        }
+    }
+
+    // Methods of the IHealable interface
+    public virtual void Heal(int gain)
+    {
+        HealthPoint = HealthPoint + gain > MaxHealth ? MaxHealth : HealthPoint + gain;
+
+        foreach (IHealthSubscriber subscriber in m_subscribers)
+        {
+            subscriber.NotifyHealthChange(this, HealthPoint);
+        }
+    }
+
+    // Override the Subscribe method
+    public override void Subscribe(IHealthSubscriber subscriber)
+    {
+        base.Subscribe(subscriber);
+        subscriber.NotifyJustSubscribed(this);
+    }
+}
---
+++ Assets/Scripts/HUD/HealthBar.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour, IHealthSubscriber
+{
+    [SerializeField]
+    private Health m_playerHealth;
+
+    private Slider m_healthBar;
+
+    private void Awake()
+    {
+        m_healthBar = GetComponentInChildren<Slider>();
+
+        if (!m_healthBar)
+        {
+            Debug.LogError("No Slider component found!");
+        }
+    }
+
+    private void Start()
+    {
+        m_playerHealth.Subscribe(this);
+    }
+
+    // Methods of the IHealthSubscriber interface
+    public void NotifyJustSubscribed(Health healthScript)
+    {
+        m_healthBar.maxValue = healthScript.MaxHealth;
+        m_healthBar.value = healthScript.MaxHealth;
+    }
+
+    public void NotifyDamageApplied(Health healthScript, int damage) { }
+
+    public void NotifyHealthChange(Health healthScript, int health)
+    {
+        m_healthBar.value = health;
+    }
+
+    public void NotifyHealthDepleted(Health healthScript) { }
+}
