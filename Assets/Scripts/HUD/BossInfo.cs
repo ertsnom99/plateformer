51cc7463d572670ed9596dfcb162556100fd57de
Kévin Duguay <kevin.duguay1@gmail.com>
2019-01-04 01:43:41 -0500

    Added boss health gauge

--- Assets/Scripts/HUD/BossInfo.cs
+++ Assets/Scripts/HUD/BossInfo.cs
@@ -1,22 +1,12 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 
-public class HealthBar : MonoBehaviour, IHealthSubscriber
+public class BossInfo : Shakable, IHealthSubscriber
 {
+    [Header("Health")]
     [SerializeField]
     private Health m_health;
 
-    private Slider m_healthBar;
-
-    private void Awake()
-    {
-        m_healthBar = GetComponentInChildren<Slider>();
-
-        if (!m_healthBar)
-        {
-            Debug.LogError("No Slider component found!");
-        }
-    }
+    private float m_healthPoint;
 
     private void Start()
     {
@@ -26,15 +16,19 @@ public class HealthBar : MonoBehaviour, IHealthSubscriber
     // Methods of the IHealthSubscriber interface
     public void NotifyJustSubscribed(Health healthScript)
     {
-        m_healthBar.maxValue = healthScript.MaxHealth;
-        m_healthBar.value = healthScript.MaxHealth;
+        m_healthPoint = healthScript.HealthPoint;
     }
 
     public void NotifyDamageApplied(Health healthScript, int damage) { }
 
     public void NotifyHealthChange(Health healthScript, int health)
     {
-        m_healthBar.value = health;
+        if (m_healthPoint > health)
+        {
+            Shake();
+        }
+
+        m_healthPoint = healthScript.HealthPoint;
     }
 
     public void NotifyHealthDepleted(Health healthScript) { }
---
+++ Assets/Scripts/HUD/HealthGauge.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class HealthGauge : MonoBehaviour, IHealthSubscriber
+{
+    [Header("Health")]
+    [SerializeField]
+    private Health m_health;
+
+    [Header("Gauge")]
+    [SerializeField]
+    private Transform m_gaugeFill;
+    [SerializeField]
+    private float m_gaugeFullRotation = .0f;
+    [SerializeField]
+    private float m_gaugeEmptyRotation = 180.0f;
+
+    private void Start()
+    {
+        m_health.Subscribe(this);
+    }
+
+    private void UpdateGauge()
+    {
+        float ZRotation = Mathf.Lerp(m_gaugeEmptyRotation, m_gaugeFullRotation, (float)m_health.HealthPoint / m_health.MaxHealth);
+        Vector3 eulerAngles = transform.rotation.eulerAngles;
+        m_gaugeFill.rotation = Quaternion.Euler(eulerAngles.x, eulerAngles.z, ZRotation);
+    }
+
+    // Methods of the IHealthSubscriber interface
+    public void NotifyJustSubscribed(Health healthScript)
+    {
+        UpdateGauge();
+    }
+
+    public void NotifyDamageApplied(Health healthScript, int damage) { }
+
+    public void NotifyHealthChange(Health healthScript, int health)
+    {
+        UpdateGauge();
+    }
+
+    public void NotifyHealthDepleted(Health healthScript) { }
+}
