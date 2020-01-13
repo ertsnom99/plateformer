d2a27228d38c7cff2964c568669c9696a8830d3d
Kévin Duguay <kevin.duguay1@gmail.com>
2020-01-13 17:40:32 -0500

    Updated UI for the arcade

---
+++ Assets/Scripts/Utilities/LoopingTranslate.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class LoopingTranslate : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _start;
+    [SerializeField]
+    private Transform _end;
+    [SerializeField]
+    private float _speed = .01f;
+
+    private void Awake()
+    {
+        if (!_start)
+        {
+            Debug.LogError("No start was set for " + GetType() + " script of " + gameObject.name + "!");
+        }
+
+        if (!_end)
+        {
+            Debug.LogError("No end was set for " + GetType() + " script of " + gameObject.name + "!");
+        }
+    }
+
+    private void Start()
+    {
+        transform.position = _start.position;
+    }
+
+    private void Update()
+    {
+        Vector3 movementDirection = (_end.position - transform.position).normalized;
+        Vector3 remainingDistance = _end.position - transform.position;
+        Vector3 movementDone = movementDirection * _speed;
+        Vector3 diff = remainingDistance - movementDone;
+
+        // Loop if reached the end
+        if (Vector3.Dot(diff, movementDirection) <= .0f)
+        {
+            transform.position = _start.position + movementDirection * diff.magnitude;
+        }
+        else
+        {
+            transform.position += movementDone;
+        }
+    }
+}
